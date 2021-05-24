24023ebf701d3a87299aa0fc9e170c2b4d9d0443
Brendan Cariota <brendan@cariota.com>
2021-05-24 00:51:22 -0500

    Refactored to work with a UI

--- main.js
+++ main.js
@@ -1,15 +1,20 @@
 // Global Variables
 let winningPhrase
 let gameWinner
-let playerScore
-let computerScore
+let playerScore = 0
+let computerScore = 0
 
+const rockBtn = document.querySelector('.rockBtn')
+const paperBtn = document.querySelector('.rockBtn')
+const scissorsBtn = document.querySelector('.scissorsBtn')
 
-// Player Choice
-const playerChoice = () => {
-    let playerChoice = window.prompt('Make you choice')
-    return playerChoice
-}
+const playersChoice = document.querySelector('.playerChoice')
+const computersChoice = document.querySelector('.computerChoice')
+
+const winnerBanner = document.querySelector('.winnerBanner')
+
+const playersScore = document.querySelector('.playerScore')
+const computersScore = document.querySelector('.computerScore')
 
 // Computer Player
 const computerPlay = () => {
@@ -25,64 +30,70 @@ const computerPlay = () => {
         computerChoice = 'Scissors'
     }
 
+    computersChoice.innerText = computerChoice
+
     return computerChoice
 }
 
+// Player Choice
+function playerChoice(e) {
+    let tempPlayerChoice
+    let tempComputerChoice
+    const playerChoice = e.target.innerText
+    playersChoice.innerText = playerChoice
+    tempPlayerChoice = playerChoice.toLowerCase()
+    let computersChoice = computerPlay()
+    tempComputerChoice = computersChoice.toLowerCase()
+    playRound(tempPlayerChoice, tempComputerChoice)
+}
+
+// Add event listeners
+const btns = document.querySelectorAll('.selectBtn')
+btns.forEach(btn => {
+    btn.addEventListener('click', playerChoice)
+})
+
+
+
 // Single Round
-const playRound = () => {
-    const newPlayerSelection = playerChoice().toLowerCase()
-    const newComputerSelection = computerPlay().toLowerCase()
+const playRound = (playerChoice, computersChoice) => {
+    const newPlayerSelection = playerChoice
+    const newComputerSelection = computersChoice
 
     if (newPlayerSelection === 'rock' && newComputerSelection === 'rock') {
-        winningPhrase = 'You Tied! Rock ties with Rock' + ' No points awarded' + ' Current Score: Player: ' + playerScore + ' Computer: ' + computerScore
+        winnerBanner.innerText = 'You Tied! Rock ties with Rock' + ' No points awarded'
     }  else if (newPlayerSelection === 'rock' && newComputerSelection === 'paper') {
         computerScore++
-        winningPhrase = 'You Lose! Rock loses to Paper' + ' The Computer won a point' + ' Current Score: Player: ' + playerScore + ' Computer: ' + computerScore
+        computersScore.innerHTML = computerScore
+        winnerBanner.innerText = 'You Lose! Rock loses to Paper' + ' The Computer won a point' 
     } else if (newPlayerSelection === 'rock' && newComputerSelection === 'scissors') {
         playerScore++
-        winningPhrase = 'You Win! Rock beats Scissors' + ' You won a point' + ' Current Score: Player: ' + playerScore + ' Computer: ' + computerScore
+        playersScore.innerHTML = playerScore
+        winnerBanner.innerText = 'You Win! Rock beats Scissors' + ' You won a point' 
     } else if (newPlayerSelection === 'paper' && newComputerSelection === 'rock') {
         playerScore++
-        winningPhrase = 'You Win! Paper beats Rock' + ' You won a point' + ' Current Score: Player: ' + playerScore + ' Computer: ' + computerScore
+        playersScore.innerHTML = playerScore
+        winnerBanner.innerText = 'You Win! Paper beats Rock' + ' You won a point'
     } else if (newPlayerSelection === 'paper' && newComputerSelection === 'paper') {
-        winningPhrase = 'You Tied! Paper ties with Paper' + ' No points awarded'
+        winnerBanner.innerText = 'You Tied! Paper ties with Paper' + ' No points awarded'
     } else if (newPlayerSelection === 'paper' && newComputerSelection === 'scissors') {
         computerScore++
-        winningPhrase = 'You Lose! Paper loses to Scissors' + ' The Computer won a point' + ' Current Score: Player: ' + playerScore + ' Computer: ' + computerScore
+        computersScore.innerHTML = computerScore
+        winnerBanner.innerText = 'You Lose! Paper loses to Scissors' + ' The Computer won a point'
     } else if (newPlayerSelection === 'scissors' && newComputerSelection === 'rock') {
         computerScore++
-        winningPhrase = 'You Lose! Scissors loses to Rock' + ' The Computer won a point' + ' Current Score: Player: ' + playerScore + ' Computer: ' + computerScore
+        computersScore.innerHTML = computerScore
+        winnerBanner.innerText = 'You Lose! Scissors loses to Rock' + ' The Computer won a point'
     } else if (newPlayerSelection === 'scissors' && newComputerSelection === 'paper') {
         playerScore++
-        winningPhrase = 'You Win! Scissors beats Paper' + ' You won a point' + ' Current Score: Player: ' + playerScore + ' Computer: ' + computerScore
+        playersScore.innerHTML = playerScore
+        winnerBanner.innerText = 'You Win! Scissors beats Paper' + ' You won a point' 
     } else if (newPlayerSelection === 'scissors' && newComputerSelection === 'scissors') {
-        winningPhrase = 'You Tied! Scissors ties with Scissors' + ' No points awarded'
+        winnerBanner.innerText = 'You Tied! Scissors ties with Scissors' + ' No points awarded'
     } else {
-        winningPhrase = "Game Broken"
+        winnerBanner.innerText = "Game Broken"
     }
 
     return winningPhrase
 }
 
-
-// Game 
-const game = () => {
-    playerScore = 0
-    computerScore = 0
-
-    for (let i = 0; i <= 4; i++) {
-        console.log(playRound())
-    }
-
-    if (playerScore === computerScore) {
-        gameWinner = 'YOU TIED'
-    } else if (playerScore < computerScore) {
-        gameWinner = 'YOU LOST'
-    } else {
-        gameWinner = 'YOU WON'
-    }
-
-    return gameWinner
-}
-
-console.log(game())
